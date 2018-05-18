9c4ae5f37834dc61e006aa9e08b9580fe803ff9c
Anton Fifindik <fifindik.av@gmail.com>
2018-05-18 13:33:06 +0300

    initial commit

---
+++ src/main/java/com/antonfifindik/bittrexapi/controllers/MainController.java
@@ -0,0 +1,12 @@
+package com.antonfifindik.bittrexapi.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping("/")
+    public String getIndexPage() {
+        return "index";
+    }
+}
